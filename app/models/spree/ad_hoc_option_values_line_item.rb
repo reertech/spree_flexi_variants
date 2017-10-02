e02ddd279949752ee19d3de0973dcce7bd7c336b
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-10-02 17:54:58 +0300

    Added ad hoc option values line item customization

--- app/models/spree/ad_hoc_option_values_line_item.rb
+++ app/models/spree/ad_hoc_option_values_line_item.rb
@@ -2,5 +2,6 @@ module Spree
   class AdHocOptionValuesLineItem  < ActiveRecord::Base
     belongs_to :ad_hoc_option_value
     belongs_to :line_item
+    has_one :ad_hoc_option_values_line_item_customization
   end
 end
