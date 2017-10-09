778ddab4739f28f5cae6e624f60fbe3331bc9772
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-10-09 18:28:26 +0300

    Add decorator methods

--- app/models/spree/ad_hoc_option_values_line_item.rb
+++ app/models/spree/ad_hoc_option_values_line_item.rb
@@ -5,5 +5,17 @@ module Spree
     has_one :ad_hoc_option_values_line_item_customization
 
     alias_attribute :customization, :ad_hoc_option_values_line_item_customization
+
+    def humanable_value
+      if customization
+        customization.value
+      else
+        ad_hoc_option_value.try(:option_value).try(:presentation)
+      end
+    end
+
+    def humanable_presentation
+      ad_hoc_option_value.try(:option_value).try(:option_type).try(:presentation)
+    end
   end
 end
