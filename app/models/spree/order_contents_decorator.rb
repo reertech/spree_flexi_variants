39686f76545bc4285b594e589f782456673c1575
Denis Gavrilin <den.gavrilin1@gmail.com>
2019-10-23 15:40:40 +0300

    Added cart service

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -10,10 +10,12 @@ module Spree
         line_item.quantity += quantity.to_i
         line_item.currency = currency unless currency.nil?
       else
-        options_params = options.is_a?(ActionController::Parameters) ? options : ActionController::Parameters.new(options.to_h)
-        opts = options_params.
-          permit(PermittedAttributes.line_item_attributes).to_h.
-          merge(currency: order.currency)
+        # options_params = options.is_a?(ActionController::Parameters) ? options : ActionController::Parameters.new(options.to_h)
+        # opts = options_params.
+        #   permit(PermittedAttributes.line_item_attributes).to_h.
+        #   merge(currency: order.currency)
+        #
+        opts = { currency: order.currency }.merge ActionController::Parameters.new(options.to_unsafe_h).to_unsafe_h
 
         line_item = order.line_items.new(quantity: quantity, variant: variant, options: opts)
 
--- app/services/spree/cart/add_item_decorator.rb
+++ app/services/spree/cart/add_item_decorator.rb
@@ -1,67 +1,60 @@
+# Almost copypaste from order_contents_decorator
 module AddItemDecorator
-  def self.prepend(base)
-    base.prepend Spree::ServiceModule::Base
-  end
-
-  def call(order:, variant:, quantity: nil, options: {})
-    ApplicationRecord.transaction do
-      run :add_to_line_item
-      run Spree::Dependencies.cart_recalculate_service.constantize
-    end
-  end
+  prepend Spree::ServiceModule::Base
 
   private
 
-  def add_to_line_item(variant, quantity, options = {})
-    line_item = grab_line_item_by_variant(variant, false, options)
-    if line_item
-      line_item.quantity += quantity.to_i
-      line_item.currency = currency unless currency.nil?
-    else
-      options_params = options.is_a?(ActionController::Parameters) ? options : ActionController::Parameters.new(options.to_h)
-      opts = options_params.
-        permit(PermittedAttributes.line_item_attributes).to_h.
-        merge(currency: order.currency)
+  def add_to_line_item(order:, variant:, quantity: nil, options: {})
+    options ||= {}
+    quantity ||= 1
 
-      line_item = order.line_items.new(quantity: quantity, variant: variant, options: opts)
+    options.permit(ad_hoc_option_values: [], product_customizations: [], ad_hoc_option_value_customizations: [])
+    options_params = options.is_a?(ActionController::Parameters) ? options : ActionController::Parameters.new(options.to_h)
+    options_params.permit(Spree::PermittedAttributes.line_item_attributes).to_h.merge(currency: order.currency)
 
-      product_customizations_values = options[:product_customizations] || []
-      line_item.product_customizations = product_customizations_values
-      product_customizations_values.each { |product_customization| product_customization.line_item = line_item }
-      product_customizations_values.map(&:save) # it is now safe to save the customizations we built
+    line_item = order.line_items.new(quantity: quantity, variant: variant)
+    line_item_created = line_item.nil?
 
-      # find, and add the configurations, if any.  these have not been fetched from the db yet.              line_items.first.variant_id
-      # we postponed it (performance reasons) until we actually know we needed them
-      ad_hoc_option_value_ids = ( !!options[:ad_hoc_option_values] ? options[:ad_hoc_option_values] : [] )
-      product_option_values = ad_hoc_option_value_ids.map do |cid|
-        AdHocOptionValue.find(cid) if cid.present?
-      end.compact
-      line_item.ad_hoc_option_values = product_option_values
+    product_customizations_values = options[:product_customizations] || []
+    line_item.product_customizations = product_customizations_values
+    product_customizations_values.each { |product_customization| product_customization.line_item = line_item }
+    product_customizations_values.map(&:save) # it is now safe to save the customizations we built
 
-      # add customizations for ad hoc options.
-      # terrible code, terrible gem (╯°□°）╯︵ ┻━┻
-      ad_hoc_option_value_customizations = ( !!options[:ad_hoc_option_value_customizations] ? options[:ad_hoc_option_value_customizations] : [] )
-      ad_hoc_option_value_customizations.map do |param|
-        value_id, customization = param.first
-        ad_hoc_li = line_item.ad_hoc_option_values_line_items.detect do |item|
-          item.ad_hoc_option_value_id == value_id
-        end
-        ad_hoc_li.ad_hoc_option_values_line_item_customization =
-          AdHocOptionValuesLineItemCustomization.new(value: customization)
-      end
+    # find, and add the configurations, if any.  these have not been fetched from the db yet.              line_items.first.variant_id
+    # we postponed it (performance reasons) until we actually know we needed them
+    ad_hoc_option_value_ids = (!!options[:ad_hoc_option_values] ? options[:ad_hoc_option_values] : [])
+    product_option_values = ad_hoc_option_value_ids.map do |cid|
+      Spree::AdHocOptionValue.find(cid) if cid.present?
+    end.compact
+    line_item.ad_hoc_option_values = product_option_values
 
-      offset_price = product_option_values.map(&:price_modifier).compact.sum + product_customizations_values.map {|product_customization| product_customization.price(variant)}.compact.sum
-
-      if currency
-        line_item.currency = currency unless currency.nil?
-        line_item.price    = variant.price_in(currency).amount + offset_price
-      else
-        line_item.price    = variant.price + offset_price
-      end
+    # add customizations for ad hoc options.
+    # terrible code, terrible gem (╯°□°）╯︵ ┻━┻
+    ad_hoc_option_value_customizations = (!!options[:ad_hoc_option_value_customizations] ? options[:ad_hoc_option_value_customizations] : [])
+    ad_hoc_option_value_customizations.map do |param|
+      value_id = param.keys.first
+      customization = param.values.first
+      ad_hoc_li =
+        line_item.ad_hoc_option_values_line_items.detect { |item| item.ad_hoc_option_value_id == value_id }
+      ad_hoc_li.ad_hoc_option_values_line_item_customization =
+        Spree::AdHocOptionValuesLineItemCustomization.new(value: customization)
     end
+
+    offset_price = product_option_values.map(&:price_modifier).compact.sum + product_customizations_values.map { |product_customization| product_customization.price(variant) }.compact.sum
+
+    if order.currency
+      line_item.currency = order.currency unless order.currency.blank?
+      line_item.price = variant.price_in(order.currency).amount + offset_price
+    else
+      line_item.price = variant.price + offset_price
+    end
+
     line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-    line_item.save!
-    line_item
+
+    return failure(line_item) unless line_item.save
+
+    ::Spree::TaxRate.adjust(order, [line_item.reload]) if line_item_created
+    success(order: order, line_item: line_item, line_item_created: line_item_created, options: options)
   end
 end
 
